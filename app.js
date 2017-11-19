04492a35ba6050e334ef724ce49e36188f5157f9
Luis Morales-Navarro <lmn297@nyu.edu>
2017-11-19 15:51:23 +0800

    added client side

--- app.js
+++ app.js
@@ -1,16 +1,97 @@
-var fs = require('fs');
-var readline = require('readline');
-var google = require('googleapis');
-var googleAuth = require('google-auth-library');
+//requirements
+var express = require("express"),
+logger = require('morgan'),
+Request = require('request'),
+fs = require('fs'),
+readline = require('readline'),
+google = require('googleapis'),
+googleAuth = require('google-auth-library');
 
-var daveID = "21142230523404";
-var luisID = "21142229562587";
+
+
+
+var daveID = "21142230523404",
+luisID = "21142229562587";
+
+
+
+
 
 var SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
     process.env.USERPROFILE) + '/.credentials/';
 var TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json';
 
+
+
+
+//Create an 'express' object
+var app = express();
+
+//Some Middleware - log requests to the terminal console
+app.use(logger('dev'));
+
+//Set up the views directory
+app.set("views", __dirname + '/views');
+//Set EJS as templating language WITH html as an extension
+app.engine('.html', require('ejs').__express);
+app.set('view engine', 'html');
+//Add connection to the public folder for css & js files
+app.use(express.static(__dirname + '/public'));
+
+/*-----
+ROUTES
+-----*/
+
+//Main Page Route - NO data
+app.get("/", function(req, res){
+  res.render('index');
+});
+
+
+//JSON Serving route
+app.get("/api/:id", function(req, res){
+  //CORS enable this route - http://enable-cors.org/server.html
+  res.header('Access-Control-Allow-Origin', "*");
+  var currentID = req.params.id;
+  var requestURL = "";
+  /*Request(requestURL, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      //console.log(body);
+      var theData = JSON.parse(body);
+      //console.log(theData);
+      //send all the data
+      res.json(theData);
+    }
+  });*/
+  res.json("success");
+});
+
+//Catch All Route
+app.get("*", function(req, res){
+  res.send('Sorry, nothing doing here.');
+});
+
+// Start the server
+app.listen(3000);
+console.log('Express started on port 3000');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 //load client secrets from a local file.
 fs.readFile("client_secret.json", function processClientSecrets(err, content){
     if(err){
@@ -18,7 +99,7 @@ fs.readFile("client_secret.json", function processClientSecrets(err, content){
         return;
     }
     authorize(JSON.parse(content), doSomeShit);
-});
+});*/
 
 function authorize(credentials, callback) {
   var clientSecret = credentials.installed.client_secret;
